f405e6f3e1ef19dae0c788d6d2f35cee49fc1497
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-01-22 08:41:08 -0800

    "Fixed render and return in App in order to successfully mount"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as $ from 'jQuery'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            name: 'Your name here',
-            message: 'Your message here'
+            name: '',
+            message: ''
         }
-        this.handleSumbit = this.handleSumbit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.nameChangeHandler = this.nameChangeHandler.bind(this);
         this.messageChangeHandler = this.messageChangeHandler.bind(this);
     }
@@ -16,36 +17,36 @@ class App extends React.Component {
     //how does event.target work? 
     nameChangeHandler(event) {
         this.setState({
-            value: event.this.state.name
+            name: event.target.value
         });
     }
 
     messageChangeHandler(event) {
         this.setState({
-            value: event.this.state.message
+            message: event.target.value
         });
     }
 
-    handleSumbit() {
-        sendMessage(this.state.name, this.state.message);
+    handleSubmit() {
+        console.log(this.state);
+        this.sendMessage(this.state.name, this.state.message);
     }
 
     sendMessage (name, message) {
-        alert('Thank you for your message ' + this.state.name);
         $.post("http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf110/greeting", {
             "name": name,
             "message": message
-          });
+          }, alert('Your message was sent. Thanks for visiting ' + this.state.name));
     } 
 
-    return () {
-        render (
+    render () {
+        return (
             <div>Response from the Server here: 
                 <div>
                     <form>
                         Name: <input type="text" id="name" value={this.state.name} onChange={this.nameChangeHandler}></input>
                         Message: <input type="text" id="message" value={this.state.message} onChange={this.messageChangeHandler}></input>
-                        <button type="button" onSubmit={() => {handleSumbit}}>Send</button>
+                        <button type="button" onClick={this.handleSubmit}>Send</button>
                     </form>
                 </div>  
             </div>
@@ -54,4 +55,4 @@ class App extends React.Component {
 
 }
 
-export default;
+export default App;
